cb4e8cce50bda8bbb978a7ae411e185663098027
Simon Pelz <simon.pelz.tobi@gmail.com>
2021-12-13 21:55:39 +0100

    Better workaround for no token

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const { BOT_TOKEN } = process.env;
-
-if (!BOT_TOKEN) process.exit(1);
+const botToken = process.env.BOT_TOKEN ?? '';
 
 import { readFileSync } from 'fs';
 import { Bot } from './src/bot';
@@ -11,4 +9,4 @@ import { Config } from './src/types/config.type';
 
 const config: Config = JSON.parse(readFileSync('./config.json', 'utf-8'));
 
-new Bot(BOT_TOKEN, config);
+new Bot(botToken, config);
